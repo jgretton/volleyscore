b4891a0f014639edc25689eb99a5b403d15f3e6b
Joshua Gretton <jb.gretton@googlemail.com>
2024-05-14 18:27:19 +0100

    Vercel Analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 // import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
         {/* <Navbar /> */}
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
