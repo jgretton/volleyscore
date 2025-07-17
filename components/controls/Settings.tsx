711cae8a21f96753612aa4755b0c3ae0c7d815aa
Joshua Gretton <jb.gretton@googlemail.com>
2025-07-17 16:53:12 +0100

    updated Settings to ts
    Updated the file to tsx, capitalised the name, added type safety to the file. fixed the onclose bug in the dialog from (!false) to (false)

--- components/controls/Settings.tsx
+++ components/controls/Settings.tsx
@@ -6,33 +6,34 @@ import {
   Transition,
   TransitionChild,
 } from "@headlessui/react";
-import { Cog6ToothIcon, CogIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Cog6ToothIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import React, { useState, Fragment } from "react";
 import DarkModeToggle from "./darkModeToggle";
 import { useGameStore } from "@/store";
+import { TeamNames } from "@/store/types";
 
 const Settings = () => {
   const { match, updateTeamName } = useGameStore();
-  const [isOpen, setIsOpen] = useState(false);
-  const [teamNames, setTeamNames] = useState({
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [teamNames, setTeamNames] = useState<TeamNames>({
     homeTeamName: match.homeTeamName,
     awayTeamName: match.awayTeamName,
   });
 
-  const changeName = (e) => {
+  const changeName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTeamNames((prevState) => {
       return {
         ...prevState,
-        [e.target.name]: e.target.value,
+        [e.target.name as keyof TeamNames]: e.target.value,
       };
     });
   };
 
   const handleTeamNameUpdate = () => {
     //If user removes all text from input, default to Home and Away
-    const validatedTeamNames = {
-      homeTeamName: teamNames.homeTeamName.trim("") || "Home",
-      awayTeamName: teamNames.awayTeamName.trim("") || "Away",
+    const validatedTeamNames: TeamNames = {
+      homeTeamName: teamNames.homeTeamName.trim() || "Home",
+      awayTeamName: teamNames.awayTeamName.trim() || "Away",
     };
 
     setTeamNames(validatedTeamNames);
@@ -41,22 +42,21 @@ const Settings = () => {
   return (
     <>
       <button
-        className="focus-visible:outline-hidden inline-flex cursor-pointer items-center gap-3 rounded-lg bg-gray-100 px-4 py-2 transition-all hover:bg-gray-200 focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-50 dark:bg-slate-800 dark:text-white dark:hover:bg-slate-900"
+        className="focus-visible:ring-opacity-50 inline-flex cursor-pointer items-center gap-3 rounded-lg bg-gray-100 px-4 py-2 transition-all hover:bg-gray-200 focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:outline-hidden dark:bg-slate-800 dark:text-white dark:hover:bg-slate-900"
         type="button"
         onClick={() => setIsOpen(!isOpen)}
       >
         <Cog6ToothIcon className="size-6" aria-hidden="true" />
       </button>
 
-      <Transition
-        appear
-        show={isOpen}
-        as={Fragment}
-        onClose={() => {
-          setIsOpen(!false);
-        }}
-      >
-        <Dialog as="div" className="relative z-10">
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => {
+            setIsOpen(false);
+          }}
+        >
           <TransitionChild
             as={Fragment}
             enter="ease-out duration-300"
@@ -84,7 +84,7 @@ const Settings = () => {
                   <div className="flex w-full flex-row items-center justify-between">
                     <DialogTitle
                       as="h3"
-                      className="text-lg font-medium leading-6 text-gray-900 dark:text-white"
+                      className="text-lg leading-6 font-medium text-gray-900 dark:text-white"
                     >
                       Settings
                     </DialogTitle>
