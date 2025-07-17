c628b4ba0816d198e17a60e0b414c09957b2ace6
Joshua Gretton <jb.gretton@googlemail.com>
2025-07-17 23:18:00 +0100

    Update MatchCompleteContent.tsx
    Modal closes when match is reset.

--- components/modal/content/MatchCompleteContent.tsx
+++ components/modal/content/MatchCompleteContent.tsx
@@ -140,6 +140,7 @@ const MatchCompleteContent = ({
         <button
           onClick={() => {
             resetMatchData();
+            closeModal();
           }}
           className="w-full rounded-lg bg-blue-600 px-4 py-3 font-medium text-white transition-colors hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
         >
